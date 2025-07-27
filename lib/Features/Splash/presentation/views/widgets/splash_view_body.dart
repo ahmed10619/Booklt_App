f285601e1f6707f61d467fde14b6cdae221a2197
Your Name <your_email@example.com>
2025-07-27 14:04:52 +0300

    change Themes $add text to splash view body & add package gap in pubspec

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -11,6 +12,11 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+        const Gap(8),
+        const Text(
+          "Read Free Books",
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
