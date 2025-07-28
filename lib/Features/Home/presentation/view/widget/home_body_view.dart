ad45b3e8196db5c7a29c1e9e2d4f015ee6fff57a
Your Name <your_email@example.com>
2025-07-28 16:00:02 +0300

    add text and add style

--- lib/Features/Home/presentation/view/widget/home_body_view.dart
+++ lib/Features/Home/presentation/view/widget/home_body_view.dart
@@ -1,18 +1,29 @@
 import 'package:bookly_app/Features/Home/presentation/view/widget/custom_appbar.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widget/featured_list_books.dart';
+import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeBodyView extends StatelessWidget {
   const HomeBodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturesListBooks(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturesListBooks(),
+          Gap(50),
+          Text(
+            "Best Saller",
+            style: Style.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
