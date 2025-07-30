45380e4484220c21480314fa90d5ce18b8e9b9a5
Your Name <your_email@example.com>
2025-07-30 14:25:19 +0300

    Best Saller List View Item part one

--- lib/Features/Home/presentation/view/widget/home_body_view.dart
+++ lib/Features/Home/presentation/view/widget/home_body_view.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/Home/presentation/view/widget/custom_appbar.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widget/featured_list_books.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ class HomeBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -22,8 +23,37 @@ class HomeBodyView extends StatelessWidget {
             "Best Saller",
             style: Style.titleMedium,
           ),
+          BestSallerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSallerListViewItem extends StatelessWidget {
+  const BestSallerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 125,
+          child: AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
