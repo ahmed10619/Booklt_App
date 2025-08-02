268fcf0d9f07dc552dc1802a3645438076c6356b
Your Name <your_email@example.com>
2025-08-02 19:05:29 +0300

    add  title in book details

--- lib/Features/Home/presentation/view/book_details_body_view.dart
+++ lib/Features/Home/presentation/view/book_details_body_view.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/Features/Home/presentation/view/widget/appbar_book_details_view.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widget/custom_book_image_item.dart';
+import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BookDetailsBodyView extends StatelessWidget {
   const BookDetailsBodyView({super.key});
@@ -16,6 +18,22 @@ class BookDetailsBodyView extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .18),
             child: const CustomBookImageItem(),
+          ),
+          const Gap(43),
+          const Text(
+            "The Jungle Book",
+            style: Style.textFont32,
+          ),
+          const Gap(6),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Style.textFont20.copyWith(
+                fontWeight: FontWeight.normal,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
           ),
         ],
       ),
