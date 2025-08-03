b395079151a4ecaf8f4ab314332f41448a765c42
Your Name <your_email@example.com>
2025-08-03 12:51:33 +0300

    add custom scroll view

--- lib/Features/Home/presentation/view/book_details_body_view.dart
+++ lib/Features/Home/presentation/view/book_details_body_view.dart
@@ -4,7 +4,6 @@ import 'package:bookly_app/Features/Home/presentation/view/widget/appbar_book_de
 import 'package:bookly_app/Features/Home/presentation/view/widget/booking_rating.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widget/custom_book_image_item.dart';
 import 'package:bookly_app/core/utils/style.dart';
-import 'package:bookly_app/core/widget/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -14,49 +13,57 @@ class BookDetailsBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          const AppBarBookDetailsView(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .18),
-            child: const CustomBookImageItem(),
-          ),
-          const Gap(43),
-          const Text(
-            "The Jungle Book",
-            style: Style.textFont32,
-          ),
-          const Gap(6),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              "Rudyard Kipling",
-              style: Style.textFont20.copyWith(
-                fontWeight: FontWeight.normal,
-                fontStyle: FontStyle.italic,
-              ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              children: [
+                const AppBarBookDetailsView(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .18),
+                  child: const CustomBookImageItem(),
+                ),
+                const Gap(38),
+                const Text(
+                  "The Jungle Book",
+                  style: Style.textFont32,
+                ),
+                const Gap(6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    "Rudyard Kipling",
+                    style: Style.textFont20.copyWith(
+                      fontWeight: FontWeight.normal,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                const Gap(18),
+                const BookingRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const Gap(20),
+                const BookActions(),
+                const Expanded(child: Gap(50)),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You Can Also Like',
+                    style: Style.textFont17,
+                  ),
+                ),
+                const Gap(16),
+                const SimilarBookListView(),
+                const Gap(16),
+              ],
             ),
           ),
-          const Gap(18),
-          const BookingRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const Gap(25),
-          const BookActions(),
-          const Gap(15),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You Can Also Like',
-              style: Style.textFont17,
-            ),
-          ),
-          const Gap(16),
-          const SimilarBookListView(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
