e5629c93485891f63f557c988ff3903525129335
Your Name <your_email@example.com>
2025-08-04 12:59:17 +0300

    create home repo method and failuers file

--- lib/Features/Home/data/repo/home_repo.dart
+++ lib/Features/Home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fecthBestSallerBooks();
-  fetchFearuredBooks();
+  Future<Either<Failures, List<BookModel>>> fecthBestSallerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFearuredBooks();
 }
