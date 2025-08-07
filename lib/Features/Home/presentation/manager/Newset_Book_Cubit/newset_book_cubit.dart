02856a2015927b09f1ec20f8597a195f9b91dbd8
Your Name <your_email@example.com>
2025-08-07 12:44:37 +0300

    newset book cubit done

--- lib/Features/Home/presentation/manager/Newset_Book_Cubit/newset_book_cubit.dart
+++ lib/Features/Home/presentation/manager/Newset_Book_Cubit/newset_book_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_book_state.dart';
 
 class NewsetBookCubit extends Cubit<NewsetBookState> {
-  NewsetBookCubit() : super(NewsetBookInitial());
+  NewsetBookCubit(this.homeRepo) : super(NewsetBookInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBook() async {
+    emit(NewsetBookLoading());
+    var result = await homeRepo.fetchFearuredBooks();
+    result.fold((failure) {
+      emit(NewsetBookFailure(failure.errorMasg));
+    }, (books) {
+      emit(NewsetBookSuccess(books));
+    });
+  }
 }
